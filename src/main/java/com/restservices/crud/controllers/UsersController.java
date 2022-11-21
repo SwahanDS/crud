ffb27799982a90b2f752481984ac8e82b5bd081f
SwahanDS <114724853+SwahanDS@users.noreply.github.com>
2022-11-21 21:03:38 +0530

    added annotation for cors policy

--- src/main/java/com/restservices/crud/controllers/UsersController.java
+++ src/main/java/com/restservices/crud/controllers/UsersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +19,7 @@ import com.restservices.crud.beans.User;
 import com.restservices.crud.services.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*", exposedHeaders = "Authorization")
 public class UsersController {
 
     @Autowired
